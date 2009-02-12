f7ce5e10a1f92a521b0e22b4457ca70e6fe95ee7
Nat Welch <nat@natwelch.com>
2009-02-12 15:35:53 -0800

    Added some config style stuff.
    Right now all this does is it gets your most recent statuses

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python
 # Ha, and you thought I'd commit code.
 
+from twitter import *;
+
+# a file with two lines. The first is your email, the second is your password
+f = open("_config")
+email =  f.readline();
+pw = f.readline();
+
+email = email.strip();
+pw = pw.strip();
+
+temp = Twitter(email,pw);
+
+for status in temp.statuses.user_timeline():
+	print status['text'];
+
